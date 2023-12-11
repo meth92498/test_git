904b2b188c490a962de0a95a43ebdc66f30ef700
meth92498 <mouhamethseck63@gmail.com>
2023-12-11 11:44:55 +0100

    ajout de fichiers text

--- Documents/test_git/test_python_data.py
+++ Documents/test_git/test_python_data.py
@@ -1,3 +1,14 @@
+# Demander à l'utilisateur son prénom
+prenom = input("Entrez votre prénom : ")
+
+# Demander à l'utilisateur son nom
+nom = input("Entrez votre nom : ")
+
+# Construire le message de salutation
+message = f"Bonjour, {prenom} {nom} ! Bienvenue en Python."
+
+# Afficher le message
+print(message)
 # Ouvrir un fichier en mode écriture
 with open('exemple.txt', 'w') as fichier:
     # Écrire des lignes dans le fichier
